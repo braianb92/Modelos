6306d8bfd6400f1be835686c3e13bb904a26c211
Baldino Braian <baldino.bab@gmail.com>
2019-06-19 21:58:40 -0300

    Add files via upload

---
+++ finalee/EjercicioPre2doParcialV2/Empleado.c
@@ -0,0 +1,6 @@
+#include "LinkedList.h"
+
+void em_calcularSueldo(void* p)
+{
+
+}
---
+++ finalee/EjercicioPre2doParcialV2/Parser.c
@@ -0,0 +1,39 @@
+#include "LinkedList.h"
+
+int parser_parseEmpleados(char* fileName, LinkedList* listaEmpleados)
+{
+    char bufferId[4096];
+    char bufferNombre[4096];
+    char bufferHorasTrabajadas[4096];
+    int cantidadDatos;
+    Employee *pEmpleado;
+    int retorno=-1;
+
+    if(pFile != NULL && pArrayListEmployee!=NULL)
+    {
+        fscanf(pFile,"%[^,],%[^,],%[^\n]\n",  bufferId,
+                                                  bufferNombre,
+                                                  bufferHorasTrabajadas);
+        do
+        {
+            cantidadDatos=fscanf(pFile,"%[^,],%[^,],%[^\n]\n",  bufferId,
+                                                  bufferNombre,
+                                                  bufferHorasTrabajadas);
+            if(cantidadDatos==3)
+            {
+                pEmpleado = employee_newParametros( bufferId,
+                                                    bufferNombre,
+                                                    bufferHorasTrabajadas);
+                if(pEmpleado != NULL)
+                {
+                    if(!ll_add(pArrayListEmployee,pEmpleado))
+                    {
+                        retorno=1;
+                    }
+                }
+            }
+        }while(!feof(pFile));
+    }
+    return retorno;
+}
+
